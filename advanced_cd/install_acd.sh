9f783e88b8f02c2b2ad7d54958526234011020f8
jaehong <dymic9065@gmail.com>
2024-03-22 12:30:57 +0900

    Add installing advanced cd

---
+++ advanced_cd/install_acd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "installing acd_cli"
+
+SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
+
+# check if acd_cli is already installed
+if [ -f ~/.acd_func.sh ]; then
+    echo "acd_cli already installed"
+    exit 0
+fi
+
+# Install acd_cli
+sudo ln -sf "$SCRIPT_PATH/acd_func.sh" ~/.acd_func.sh
+
+# Add acd_cli to bashrc
+echo "source ~/.acd_func.sh" >> ~/.bashrc
