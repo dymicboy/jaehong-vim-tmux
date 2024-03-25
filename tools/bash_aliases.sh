1732afc88868d1d00cf1fd5c82e21da1d4c4f41a
jaehong <dymic9065@gmail.com>
2024-03-25 13:06:27 +0900

    fix bug in bash_aliases.sh

--- tools/bash_aliases.sh
+++ tools/bash_aliases.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Shows pid of a process that has a given port open
-lsof_port() { sudo lsof -i :"$1"; }
+lsof_port(){ sudo lsof -i :"$1"; }
 
 # Shows list of files that contain a given string
-af() { ag -g "$1";}
+af(){ ag -g "$1";}
 
 # Shows list of branches sorted by committer date
 alias git_branch="git branch --sort=-committerdate"
 
 # Refreshes the bashrc file
-rebash() {
+rebash(){
     source ~/.bashrc
 }
