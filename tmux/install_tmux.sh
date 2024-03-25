b1a889f815f11da7eaf5cee64066a0a0c5a708ee
jaehong <dymic9065@gmail.com>
2024-03-25 13:01:38 +0900

    tmux_gpkosz

--- tmux/install_tmux.sh
+++ tmux/install_tmux.sh
@@ -8,8 +8,8 @@ sudo apt-get install tmux
 # Get script path
 SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
 
-# Make .tmux.conf as a symbolic link of gpkosz_tmux/.tmux.conf
-sudo ln -sf "$SCRIPT_PATH/third_parties/gpkosz_tmux/.tmux.conf" ~/.tmux.conf
+# Make .tmux.conf as a symbolic link of tmux_gpkosz/.tmux.conf
+sudo ln -sf "$SCRIPT_PATH/third_parties/tmux_gpkosz/.tmux.conf" ~/.tmux.conf
 
 # Make .tmux.conf.local as a symbolic link of tmux.conf.local
 sudo ln -sf "$SCRIPT_PATH/tmux.conf.local" ~/.tmux.conf.local
